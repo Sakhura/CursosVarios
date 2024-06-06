c4a0f2c426505b56e39af0e2ecb1d1b2ece7077e
sakhura <sabina.romero@gmail.com>
2024-06-06 16:38:41 -0400

    Desarrollo ejercicios básico Java SE 2

---
+++ JavaSE/untitled/src/EstructuraCondicionalAnidada5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class EstructuraCondicionalAnidada5 {
+
+    public static void main(String[] ar) {
+        Scanner teclado=new Scanner(System.in);
+        int totalPreguntas,totalCorrectas;
+        System.out.print("Ingrese la cantidad total de preguntas del examen:");
+        totalPreguntas=teclado.nextInt();
+        System.out.print("Ingrese la cantidad total de preguntas contestadas correctamente:");
+        totalCorrectas=teclado.nextInt();
+        int porcentaje=totalCorrectas * 100 / totalPreguntas;
+        if (porcentaje>=90) {
+            System.out.print("Nivel máximo");
+        } else {
+            if (porcentaje>=75) {
+                System.out.print("Nivel medio");
+            } else {
+                if (porcentaje>=50) {
+                    System.out.print("Nivel regular");
+                } else {
+                    System.out.print("Fuera de nivel");
+                }
+            }
+        }
+    }
+}
