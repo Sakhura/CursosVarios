03f4072c144d817129e0b2f610e0b1c84130dd66
sakhura <sabina.romero@gmail.com>
2024-06-06 18:49:06 -0400

    datos tipo vector2

--- JavaSE/untitled/src/DatosTipoVector/PruebaVector8.java
+++ JavaSE/untitled/src/DatosTipoVector/PruebaVector8.java
@@ -1,2 +1,33 @@
-package DatosTipoVector;public class PruebaVector8 {
+package DatosTipoVector;
+
+import java.util.Scanner;
+
+public class PruebaVector8 {
+    private Scanner teclado;
+    private int[] sueldos;
+
+    public void cargar()
+    {
+        teclado=new Scanner(System.in);
+        System.out.print("Cuantos sueldos cargará:");
+        int cant;
+        cant=teclado.nextInt();
+        sueldos=new int[cant];
+        for(int f=0;f<sueldos.length;f++) {
+            System.out.print("Ingrese sueldo:");
+            sueldos[f]=teclado.nextInt();
+        }
+    }
+
+    public void imprimir() {
+        for(int f=0;f<sueldos.length;f++) {
+            System.out.println(sueldos[f]);
+        }
+    }
+
+    public static void main(String[] ar) {
+        PruebaVector8 pv=new PruebaVector8();
+        pv.cargar();
+        pv.imprimir();
+    }
 }
