dc57787ec481b9768163492cecd2b662feb6fd63
sakhura <sabina.romero@gmail.com>
2024-06-06 18:04:33 -0400

    Orden package

--- JavaSE/untitled/src/ClasesObjetos/Cuadrado.java
+++ JavaSE/untitled/src/ClasesObjetos/Cuadrado.java
@@ -1,2 +1,34 @@
-package ClasesObjetos;public class Cuadrado {
+package ClasesObjetos;
+
+import java.util.Scanner;
+
+public class Cuadrado {
+    private Scanner teclado;
+    int lado;
+
+    public void inicializar() {
+        teclado=new Scanner(System.in);
+        System.out.print("Ingrese valor del lado :");
+        lado=teclado.nextInt();
+    }
+
+    public void imprimirPerimetro() {
+        int perimetro;
+        perimetro=lado*4;
+        System.out.println("El perímetro es:"+perimetro);
+    }
+
+    public void imprimirSuperficie() {
+        int superficie;
+        superficie=lado*lado;
+        System.out.println("La superficie es:"+superficie);
+    }
+
+    public static void main(String[] ar) {
+        Cuadrado cuadrado1;
+        cuadrado1=new Cuadrado();
+        cuadrado1.inicializar();
+        cuadrado1.imprimirPerimetro();
+        cuadrado1.imprimirSuperficie();
+    }
 }
